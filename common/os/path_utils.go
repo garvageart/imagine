0ffd7c95c2ddf659003d0aef8e264824cc9ed21b
Les <45718863+garvageart@users.noreply.github.com>
2025-05-29 04:03:39 +0200

    added variable for getting the current working directory

--- common/os/path_utils.go
+++ common/os/path_utils.go
@@ -1,6 +1,22 @@
 package libos
 
-import "strings"
+***REMOVED***
+***REMOVED***
+***REMOVED***
+	"strings"
+***REMOVED***
+
+var (
+	CurrentWorkingDirectory = func(***REMOVED*** string {
+		cwd, err := os.Getwd(***REMOVED***
+
+	***REMOVED***
+			panic(fmt.Errorf("error retrieving current working directory: %w", err***REMOVED******REMOVED***
+	***REMOVED***
+
+		return StandardisePaths(cwd***REMOVED***
+***REMOVED***(***REMOVED***
+***REMOVED***
 
 // Microsoft you will pay for your crimes against standards
 func StandardisePaths(path string***REMOVED*** string {
