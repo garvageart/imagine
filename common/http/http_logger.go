6248054c1ba4b9db712068088593726f13a9e98c
Les <45718863+garvageart@users.noreply.github.com>
2025-05-29 19:36:37 +0200

    add server key to logger

--- common/http/http_logger.go
+++ common/http/http_logger.go
@@ -1,14 +1,17 @@
 package http
 
 ***REMOVED***
+***REMOVED***
 	"log/slog"
 ***REMOVED***
+	"strings"
 
 	imalog "imagine/log"
+
 	"go.les-is.online/imagine/utils"
 ***REMOVED***
 
-func setupChiLogHandler(***REMOVED*** []slog.Handler {
+func setupChiLogHandler(name string***REMOVED*** []slog.Handler {
 	httpLogFileDefaults := imalog.LogFileDefaults
 	logLevel := func(***REMOVED*** slog.Level {
 	***REMOVED***
@@ -20,13 +23,14 @@ func setupChiLogHandler(***REMOVED*** []slog.Handler {
 	fileHandler := imalog.NewFileLogger(&imalog.ImalogHandlerOptions{
 		Writer: imalog.FileLog{
 			Directory: httpLogFileDefaults.Directory + "/http",
-			Filename:  httpLogFileDefaults.Filename + "-http",
+			Filename:  fmt.Sprintf("%s-http-%s", httpLogFileDefaults.Filename, strings.ReplaceAll(name, "-", "_"***REMOVED******REMOVED***,
 ***REMOVED***
 		ShowSource: true,
 		HandlerOptions: &slog.HandlerOptions{
 			Level: logLevel,
 			// Strip all ANSI codes from the log output set by the
-			// go-chi logger middleware
+			// go-chi logger middleware. Even if colour logging is disabled is production
+			// during development the middleware wraps strings in colour
 			ReplaceAttr: func(groups []string, a slog.Attr***REMOVED*** slog.Attr {
 				if a.Key == slog.MessageKey {
 					a.Value = slog.StringValue(imalog.StripAnsi(a.Value.String(***REMOVED******REMOVED******REMOVED***
@@ -52,8 +56,8 @@ func setupChiLogHandler(***REMOVED*** []slog.Handler {
 ***REMOVED***
 ***REMOVED***
 
-func SetupChiLogger(***REMOVED*** *slog.Logger {
-	handlers := setupChiLogHandler(***REMOVED***
+func SetupChiLogger(name string***REMOVED*** *slog.Logger {
+	handlers := setupChiLogHandler(name***REMOVED***
 
 	logger := imalog.CreateLogger(handlers***REMOVED***
 	return logger
