f84738fe7938cae80db076f59d2b305622286e43
Les <45718863+garvageart@users.noreply.github.com>
2025-06-03 15:18:57 +0200

    fix server error logging

--- common/http/errors.go
+++ common/http/errors.go
@@ -5,10 +5,11 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/render"
 )
 
-func ServerError(res http.ResponseWriter, req *http.Request, err error, logger *slog.Logger, logArgs *slog.Attr, msg string, userMsg string) {
+func ServerError(res http.ResponseWriter, req *http.Request, err error, logger *slog.Logger, logArgs []slog.Attr, msg string, userMsg string) {
 	jsonResponse := map[string]any{
 		"user_message": "something went wrong on our side, please try again later",
 		"message":      msg,
@@ -17,26 +18,31 @@ func ServerError(res http.ResponseWriter, req *http.Request, err error, logger *
 	msg = strings.TrimSpace(msg)
 	userMsg = strings.TrimSpace(userMsg)
 
+	if logArgs == nil {
+		logArgs = []slog.Attr{}
+	}
+
+	// Get the request ID
+	logArgs = append(logArgs, slog.String("request_id", middleware.GetReqID(req.Context())))
+
+	// Add the user message to the response if it's not blank
 	if userMsg != "" {
 		jsonResponse["user_message"] = userMsg
+		logArgs = append(logArgs, slog.String("user_message", userMsg))
 	}
 
+	// If the server-specific error message is blank, return
+	// the error as a string
 	if msg != "" {
 		jsonResponse["message"] = msg
+		logArgs = append(logArgs, slog.String("message", msg))
 	} else {
-		msg = userMsg
+		msg = err.Error()
 	}
 
-	if msg != "" {
-		jsonResponse["error"] = err.Error()
-	}
+	logger.Error(msg, slog.Any("args", logArgs))
 
-	if logArgs == nil {
-		logArgs = &slog.Attr{}
-	}
-
-	logger.Error(msg, *logArgs, slog.String("err", err.Error()))
-
+	// Usually theres no other kind of http error code for stuff breaking on the server
 	res.WriteHeader(http.StatusInternalServerError)
 	render.JSON(res, req, jsonResponse)
 }
