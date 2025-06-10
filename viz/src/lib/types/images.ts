ea73d219ba7ead845b149d9d58d26fa1348b9d82
Les <45718863+garvageart@users.noreply.github.com>
2025-06-10 15:46:39 +0200

    add note about supported files

--- viz/src/lib/types/images.ts
+++ viz/src/lib/types/images.ts
@@ -1,4 +1,14 @@
+/**
+ * Taken from https://docs.photoprism.app/developer-guide/media/raw/
+ * 
+ * Not the final supported files, this may eventually end up being removed
+*/
 export type SupportedImageTypes = "jpeg" | "jpg" | "png" | "tiff";
+/**
+ * Taken from https://docs.photoprism.app/developer-guide/media/raw/
+ * 
+ * Not the final supported files, this may eventually end up being removed
+*/
 export type SupportedRAWFiles = "3fr" | "ari" | "arw" | "bay" | "cap" | "cr2" | "cr3" | "crw" | "data" | "dcr" | "dcs" | "drf" | "eip" | "erf" | "fff" | "gpr" | "iiq" | "k25" | "kdc" | "mdc" | "mef" | "mos" | "mrw" | "nef" | "nrw" | "obm" | "orf" | "pef" | "ptx" | "pxn" | "r3d" | "raf" | "raw" | "rw2" | "rwl" | "rwz" | "sr2" | "srf" | "srw" | "x3f";
 
 export interface ImageObjectData {
