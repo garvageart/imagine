02dd682e297ab5473c8bd232e339f0aabe83d5ca
Les <45718863+garvageart@users.noreply.github.com>
2025-06-10 15:07:50 +0200

    add default styles for material icon

--- viz/src/lib/components/MaterialIcon.svelte
+++ viz/src/lib/components/MaterialIcon.svelte
@@ -1,5 +1,25 @@
 <script lang="ts">
-	let { iconName }: { iconName: string } = $props();
+	import type { SvelteHTMLElements } from "svelte/elements";
+
+	let { iconName, ...props }: { iconName: string } & SvelteHTMLElements["span"] = $props();
 </script>
 
-<span class="material-symbols-sharp"> {iconName} </span>
+<span class="material-symbols-sharp" {...props}> {iconName} </span>
+
+<style lang="scss">
+	.material-symbols-sharp {
+		transition: all 150ms linear;
+		padding: 0.1em;
+		margin: 0.2em;
+		display: inline-block;
+		border-radius: 100%;
+
+		&:hover {
+			background-color: var(--imag-blue-90);
+		}
+
+		&:active {
+			background-color: var(--imag-blue-80);
+		}
+	}
+</style>
