022a709f794ae23f694e7826da1248d655783608
Les <45718863+garvageart@users.noreply.github.com>
2025-06-10 15:01:22 +0200

    fix bracket

--- viz/e2e/demo.test.ts
+++ viz/e2e/demo.test.ts
@@ -2,5 +2,5 @@ import { expect, test, Page } from '@playwright/test';
 
 test('home page has expected h1', async ({ page }: { page: Page }) => {
 	await page.goto('/');
-	await expect(page.locator('h1').toBeVisible());
+	await expect(page.locator('h1')).toBeVisible();
 });
