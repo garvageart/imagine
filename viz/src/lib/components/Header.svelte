dfbeee13132b29b2b003853ed563f249214c52f9
Les <45718863+garvageart@users.noreply.github.com>
2025-06-13 00:39:01 +0200

    adjust header size

--- viz/src/lib/components/Header.svelte
+++ viz/src/lib/components/Header.svelte
@@ -1,39 +1,24 @@
 <script lang="ts">
-	import { sidebar } from "$lib/states/index.svelte";
-	import MaterialIcon from "./MaterialIcon.svelte";
 </script>
 
 <header>
 	<a id="viz-title" href="/">viz</a>
-	<button
-		id="viz-menu-icon"
-		onclick={() => {
-			sidebar.open = !sidebar.open;
-		}}
-	>
-		<MaterialIcon iconName="menu" />
-	</button>
 </header>
 
 <style>
 	header {
 		background-color: var(--imag-blue-100);
-		height: 2.5em;
+		height: 1.5em;
 		padding: 0.5em 1.5em;
 		display: flex;
 		align-items: center;
 		border-bottom: 1px solid var(--imag-blue-20);
+		position: relative;
 	}
 
 	#viz-title {
 		font-family: var(--imag-code-font);
 		font-weight: 700;
-		font-size: 2em;
-	}
-
-	#viz-menu-icon {
-		display: inline-flex;
-		margin-left: auto;
-		cursor: pointer;
+		font-size: 1.5em;
 	}
 </style>
