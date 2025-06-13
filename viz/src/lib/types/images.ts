99caa1719ba083aed085d49348528e9e838e8b30
Les <45718863+garvageart@users.noreply.github.com>
2025-06-14 01:11:33 +0200

    add arrays for supported files types

--- viz/src/lib/types/images.ts
+++ viz/src/lib/types/images.ts
@@ -1,15 +1,59 @@
-/**
- * Taken from https://docs.photoprism.app/developer-guide/media/raw/
- * 
- * Not the final supported files, this may eventually end up being removed
-*/
 export type SupportedImageTypes = "jpeg" | "jpg" | "png" | "tiff";
+export const SUPPORTED_IMAGE_TYPES: SupportedImageTypes[] = [
+    "jpeg",
+    "jpg",
+    "png",
+    "tiff"
+];
+
 /**
  * Taken from https://docs.photoprism.app/developer-guide/media/raw/
  * 
  * Not the final supported files, this may eventually end up being removed
 */
 export type SupportedRAWFiles = "3fr" | "ari" | "arw" | "bay" | "cap" | "cr2" | "cr3" | "crw" | "data" | "dcr" | "dcs" | "drf" | "eip" | "erf" | "fff" | "gpr" | "iiq" | "k25" | "kdc" | "mdc" | "mef" | "mos" | "mrw" | "nef" | "nrw" | "obm" | "orf" | "pef" | "ptx" | "pxn" | "r3d" | "raf" | "raw" | "rw2" | "rwl" | "rwz" | "sr2" | "srf" | "srw" | "x3f";
+export const SUPPORTED_RAW_FILES: SupportedRAWFiles[] = [
+    "3fr",
+    "ari",
+    "arw",
+    "bay",
+    "cap",
+    "cr2",
+    "cr3",
+    "crw",
+    "data",
+    "dcr",
+    "dcs",
+    "drf",
+    "eip",
+    "erf",
+    "fff",
+    "gpr",
+    "iiq",
+    "k25",
+    "kdc",
+    "mdc",
+    "mef",
+    "mos",
+    "mrw",
+    "nef",
+    "nrw",
+    "obm",
+    "orf",
+    "pef",
+    "ptx",
+    "pxn",
+    "r3d",
+    "raf",
+    "raw",
+    "rw2",
+    "rwl",
+    "rwz",
+    "sr2",
+    "srf",
+    "srw",
+    "x3f"
+];
 
 export interface ImageObjectData {
     id: string;
@@ -27,7 +71,7 @@ export interface ImageData {
     file_name: string;
     file_size: number;
     original_file_name: string;
-    file_type: SupportedImageTypes;
+    file_type: SupportedImageTypes | SupportedRAWFiles;
     keywords: string[];
     width: number;
     height: number;
