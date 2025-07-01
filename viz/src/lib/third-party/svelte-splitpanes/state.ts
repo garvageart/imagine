cdbc8924791e54561de8d66634ab261b621d1766
Les <45718863+garvageart@users.noreply.github.com>
2025-07-01 12:55:52 +0200

    add store for layout

--- viz/src/lib/third-party/svelte-splitpanes/state.ts
+++ viz/src/lib/third-party/svelte-splitpanes/state.ts
@@ -1,8 +1,9 @@
 import { SvelteMap } from "svelte/reactivity";
 import type { IPaneSerialized } from ".";
-import { writable } from "svelte/store";
-import type { VizTab } from "$lib/components/panels/SubPanel.svelte";
+import { writable, type Writable } from "svelte/store";
+import type { VizSubPanel, VizTab } from "$lib/components/panels/SubPanel.svelte";
 
 // this might cause bugs idk
 export const allSplitpanes = writable(new SvelteMap<string, IPaneSerialized[]>());
-export const allTabs = writable(new SvelteMap<string, VizTab[]>());
+export const layoutState: Writable<VizSubPanel[]> = writable([]);
+export const allTabs = writable(new SvelteMap<string, VizTab[]>([]));
