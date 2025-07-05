fcf2ea60a870326146405642f87045d65501b42a
Les <45718863+garvageart@users.noreply.github.com>
2025-07-05 19:46:02 +0200

    move global window declarations to root layout.svelte file

--- viz/src/routes/+layout.svelte
+++ viz/src/routes/+layout.svelte
@@ -1,11 +1,34 @@
+<script module lang="ts">
+	declare global {
+		interface Window {
+			debug?: boolean;
+			___vizConfig?: VizConfig;
+			resetAndReloadLayout?: () => void;
+			__APP_VERSION__: string;
+		}
+	}
+</script>
+
 <script>
+	import { dev } from "$app/environment";
+
+	import { DEFAULT_THEME } from "$lib/constants";
+	import type { VizConfig } from "$lib/types/config.types";
 	import Header from "$lib/components/Header.svelte";
 	import "$lib/styles/scss/main.scss";
 	import "@fontsource-variable/manrope";
 
+	window.___vizConfig = {
+		environment: dev ? "dev" : "prod",
+		// @ts-ignore
+		version: __APP_VERSION__ as string,
+		debug: window.debug ?? false,
+		theme: DEFAULT_THEME
+	};
+
 	let { children } = $props();
 </script>
 
-<Header></Header>
+<Header />
 
 {@render children()}
--- viz/vite.config.ts
+++ viz/vite.config.ts
@@ -1,10 +1,19 @@
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
 import devtoolsJson from "vite-plugin-devtools-json";
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = fs.readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);
 
 export default defineConfig({
 	plugins: [devtoolsJson(), sveltekit()],
+	define: {
+		'__APP_VERSION__': JSON.stringify(pkg.version),
+	},
 	test: {
 		workspace: [
 			{
