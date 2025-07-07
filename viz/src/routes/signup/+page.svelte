eefa640e90a7cbf9fd41dc3418ad99af83fed0de
Les <45718863+garvageart@users.noreply.github.com>
2025-07-07 14:39:13 +0200

    navigate to root page if no login state on sign up

--- viz/src/routes/signup/+page.svelte
+++ viz/src/routes/signup/+page.svelte
@@ -1,4 +1,13 @@
-<a href="/">
-    Home
-</a>
+<script lang="ts">
+	import { goto } from "$app/navigation";
+	import { login } from "$lib/states/index.svelte";
+
+	const loginState = login.state;
+
+	if (!loginState) {
+		goto("/login");
+	}
+</script>
+
+<a href="/"> Home </a>
 <h1>Finish Imagine account setup</h1>
