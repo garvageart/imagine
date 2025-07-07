d38c94e0801d689e34993ef54c83d9be0b04d34c
Les <45718863+garvageart@users.noreply.github.com>
2025-07-07 15:51:55 +0200

    click header to enable/disable

--- viz/src/lib/components/Header.svelte
+++ viz/src/lib/components/Header.svelte
@@ -1,7 +1,38 @@
 <script lang="ts">
+	import { dev } from "$app/environment";
+	import { CLIENT_IS_PRODUCTION } from "$lib/constants";
+	import { VizLocalStorage } from "$lib/utils";
+
+	// eventually this will move to a different page with a different way of enabling, this is just temporary
+	let devEnabled = $state(false);
+	let devEnableClickCounter = $state(0);
+	const devEnabledValue = 10;
+	const storeDebug = new VizLocalStorage<boolean>("debugMode");
+
+	// i'd probably forget to remove this in prod setting so just check lmao
+	if (dev || !CLIENT_IS_PRODUCTION) {
+		$effect(() => {
+			if (devEnableClickCounter === devEnabledValue && !devEnabled) {
+				devEnabled = true;
+				devEnableClickCounter = 0;
+				window.debug = true;
+				storeDebug.set(true);
+
+				console.log("Debug mode enabled through header click");
+			} else if (devEnableClickCounter === devEnabledValue && devEnabled) {
+				devEnabled = false;
+				devEnableClickCounter = 0;
+				window.debug = false;
+				storeDebug.set(false);
+
+				console.log("Debug mode disabled");
+			}
+		});
+	}
 </script>
 
-<header>
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<header role="button" tabindex="0" ontouchstart={() => devEnableClickCounter++} onclick={() => devEnableClickCounter++}>
 	<a id="viz-title" href="/">viz</a>
 </header>
 
