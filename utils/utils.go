1fe48178a38a71d16dc3b657d8081d1eba2025ca
Les <45718863+garvageart@users.noreply.github.com>
2025-07-30 00:29:56 +0200

    add `StructToMap` function

--- utils/utils.go
+++ utils/utils.go
@@ -2,9 +2,10 @@ package utils
 
 import (
 	"encoding/json"
-	"os"
 	"log"
 	"maps"
+	"os"
+	"reflect"
 	"slices"
 	"strings"
 
@@ -87,4 +88,34 @@ func JsonToMap(jsonString string) (map[string]any, error) {
 	}
 
 	return result, err
+}
+
+// From https://gist.github.com/bxcodec/c2a25cfc75f6b21a0492951706bc80b8
+func StructToMap(item interface{}) map[string]interface{} {
+	res := map[string]interface{}{}
+	if item == nil {
+		return res
+	}
+
+	v := reflect.TypeOf(item)
+	reflectValue := reflect.ValueOf(item)
+	reflectValue = reflect.Indirect(reflectValue)
+
+	if v.Kind() == reflect.Ptr {
+		v = v.Elem()
+	}
+
+	for i := 0; i < v.NumField(); i++ {
+		tag := v.Field(i).Tag.Get("json")
+		field := reflectValue.Field(i).Interface()
+		if tag != "" && tag != "-" {
+			if v.Field(i).Type.Kind() == reflect.Struct {
+				res[tag] = StructToMap(field)
+			} else {
+				res[tag] = field
+			}
+		}
+	}
+	
+	return res
 }
