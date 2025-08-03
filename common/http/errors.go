bf5c0a36d2ff1d482979283eb92dd62f8fdc834d
Les <45718863+garvageart@users.noreply.github.com>
2025-08-04 00:14:58 +0200

    merge errors into one file

--- common/http/errors.go
+++ common/http/errors.go
@@ -9,6 +9,26 @@ import (
 	"github.com/go-chi/render"
 )
 
+var (
+	ErrTokenInvalid = CreateDefaultMessage("invalid token", 1000)
+	ErrTokenExpired = CreateDefaultMessage("token expired", 1001)
+	ErrTokenMissing = CreateDefaultMessage("token missing", 1002)
+	ErrRequestBodyInvalid = CreateDefaultMessage("invalid request body", 1003)
+	ErrSomethingWentWrongServer = CreateDefaultMessage("something went wrong", 1004)
+)
+
+type DefaultMessage struct {
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+}
+
+func CreateDefaultMessage(message string, code int) DefaultMessage {
+	return DefaultMessage{
+		Message: message,
+		Code:    code,
+	}
+}
+
 type ErrorResponse struct {
 	Message string `json:"message"`
 	Status  int    `json:"status"`
