f6bd4615cae3015f0fda851d93cf75ff63e64855
Les <45718863+garvageart@users.noreply.github.com>
2025-09-24 10:41:34 +0200

    remove server hooks

--- viz/src/hooks.server.ts
+++
@@ -1,9 +0,0 @@
-import type { Handle } from '@sveltejs/kit';
-
-export const handle: Handle = async ({ event, resolve }) => {
-    event.locals.user = (await event.fetch("https://localhost:7777/user")).json();
-
-    const response = await resolve(event);
-
-    return response;
-};
